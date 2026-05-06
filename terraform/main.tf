d82893e710efe8d0a2c9f722cbd16bf357f4260c
Lavanyabn1997 <varshalavanya24@gmail.com>
2026-05-06 11:33:59 +0530

    Create main.tf

---
+++ terraform/main.tf
@@ -0,0 +1,40 @@
+module "vpc" {
+  source = "./modules/vpc"
+}
+
+module "ecr" {
+  source    = "./modules/ecr"
+  repo_name = var.repo_name
+}
+
+module "iam" {
+  source = "./modules/iam"
+}
+
+module "ec2" {
+  source        = "./modules/ec2"
+  iam_profile   = module.iam.ec2_profile
+}
+
+module "codedeploy" {
+  source = "./modules/codedeploy"
+  role   = module.iam.codedeploy_role
+  ec2_id = module.ec2.instance_id
+}
+
+module "codebuild" {
+  source   = "./modules/codebuild"
+  repo_url = module.ecr.repo_url
+  role     = module.iam.codebuild_role
+}
+
+module "codepipeline" {
+  source        = "./modules/codepipeline"
+  github_owner  = var.github_owner
+  github_repo   = var.github_repo
+  github_token  = var.github_token
+
+  build_project = module.codebuild.project_name
+  deploy_app    = module.codedeploy.app_name
+  deploy_group  = module.codedeploy.group_name
+}
