b186596d51381cbecd6691d5fa607e9ed431dc48
Lavanyabn1997 <varshalavanya24@gmail.com>
2026-05-06 12:24:09 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,35 +1,35 @@
 module "vpc" {
-  source = "./modules/vpc"
+  source = "../modules/vpc"
 }
 
 module "ecr" {
-  source    = "./modules/ecr"
+  source    = "../modules/ecr"
   repo_name = var.repo_name
 }
 
 module "iam" {
-  source = "./modules/iam"
+  source = "../modules/iam"
 }
 
 module "ec2" {
-  source        = "./modules/ec2"
+  source        = "../modules/ec2"
   iam_profile   = module.iam.ec2_profile
 }
 
 module "codedeploy" {
-  source = "./modules/codedeploy"
+  source = "../modules/codedeploy"
   role   = module.iam.codedeploy_role
   ec2_id = module.ec2.instance_id
 }
 
 module "codebuild" {
-  source   = "./modules/codebuild"
+  source   = "../modules/codebuild"
   repo_url = module.ecr.repo_url
   role     = module.iam.codebuild_role
 }
 
 module "codepipeline" {
-  source        = "./modules/codepipeline"
+  source        = "../modules/codepipeline"
   github_owner  = var.github_owner
   github_repo   = var.github_repo
   github_token  = var.github_token
