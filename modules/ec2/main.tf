59e0923124fb0375ca3b8e4b026be3f3e81814f9
Lavanyabn1997 <varshalavanya24@gmail.com>
2026-05-06 13:00:21 +0530

    Update main.tf

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -1,7 +1,50 @@
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  owners      = ["099720109477"]
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+resource "aws_security_group" "nginx_sg" {
+  name        = "nginx-sg"
+  description = "Allow HTTP and SSH"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 resource "aws_instance" "app" {
-  ami                    = var.ami_id
-  instance_type          = "t2.micro"
-  iam_instance_profile   = var.iam_profile
+  ami                         = data.aws_ami.ubuntu.id
+  instance_type               = "t2.micro"
+  iam_instance_profile        = var.iam_profile
+  associate_public_ip_address = true
+
+  vpc_security_group_ids = [aws_security_group.nginx_sg.id]
 
   user_data = file("${path.module}/user_data.sh")
 
