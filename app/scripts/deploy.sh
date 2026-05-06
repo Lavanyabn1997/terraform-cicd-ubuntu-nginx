c2de1c4bdffc20551c91c909dd47ba86cb1513cd
Lavanyabn1997 <varshalavanya24@gmail.com>
2026-05-06 13:18:20 +0530

    Update deploy.sh

--- app/scripts/deploy.sh
+++ app/scripts/deploy.sh
@@ -1,18 +1,11 @@
 #!/bin/bash
 
-REGION="ap-south-1"
-REPO_URI="$REPO_URI"
+echo "Stopping existing container..."
+docker stop nginx || true
+docker rm nginx || true
 
-echo "Stopping container..."
-docker stop nginx-app || true
-docker rm nginx-app || true
+echo "Pulling latest image..."
+docker pull nginx
 
-echo "Login to ECR..."
-aws ecr get-login-password --region $REGION \
-| docker login --username AWS --password-stdin $REPO_URI
-
-echo "Pulling image..."
-docker pull $REPO_URI:latest
-
-echo "Running container..."
-docker run -d -p 80:80 --name nginx-app $REPO_URI:latest
+echo "Running new container..."
+docker run -d --name nginx -p 80:80 nginx
