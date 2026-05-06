f70c8a31a67be22408bdf937d4fd8f672039a5ad
Lavanyabn1997 <varshalavanya24@gmail.com>
2026-05-06 13:37:40 +0530

    Update deploy.sh

--- app/scripts/deploy.sh
+++ app/scripts/deploy.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
-
-echo "Stopping existing container..."
 docker stop nginx || true
 docker rm nginx || true
-
-echo "Pulling latest image..."
 docker pull nginx
-
-echo "Running new container..."
 docker run -d --name nginx -p 80:80 nginx
